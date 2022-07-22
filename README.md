02f0e1d1d1a68745d5b012ee9dab5c3bbcfad8df
triplek24 <karthikeyanrym@gmail.com>
2022-07-22 18:39:02 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Day 28 task Guvi , Add to cart and disable Cart 
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+export function Footer() {
+    return (
+      <div className="footer">
+        <p
+          style={{
+            marginBottom: "0",
+          }}
+        >
+          Copyright © Karthi Website 2022
+        </p>
+      </div>
+    );
+  }
